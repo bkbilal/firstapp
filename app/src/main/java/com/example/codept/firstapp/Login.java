7c215f45673a883ab24b408a1addc37a68f09c99
CODept <bk67967@gmail.com>
2017-03-12 13:00:15 +0530

    we adding android file

---
+++ app/src/main/java/com/example/codept/firstapp/Login.java
@@ -0,0 +1,47 @@
+package com.example.codept.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    private static EditText username,password;
+    private static Button loginbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login();
+    }
+    public void login(){
+
+        username=(EditText)findViewById(R.id.t1);
+        password=(EditText)findViewById(R.id.t2);
+        loginbtn=(Button)findViewById(R.id.lbutton);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String a1=username.getText().toString();
+                String a2=password.getText().toString();
+                if(a1.equals("bilal") && a2.equals("bilal")){
+                    Toast.makeText(getApplicationContext(), "Succefully loigin", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+
+                    Toast.makeText(getApplicationContext(), "You are not login try agin", Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+        });
+
+    }
+
+}
